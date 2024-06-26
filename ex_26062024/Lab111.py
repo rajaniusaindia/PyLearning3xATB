d10114cb24a3f93b56d8a5528909706a684a9e1d
rajanichoudhary <rajaniusaindia@gmail.com>
2024-06-26 16:40:19 -0400

    June 24 Lab111 Assignment & class work

---
+++ ex_26062024/Lab111.py
@@ -0,0 +1,17 @@
+# OOPS - Object Priented Programming
+# Object ??
+
+# Procedural PL -> Functions only - no attributes or behavior
+# C, COBOL, PASCAL
+
+# Students -> Attributes, Behavior
+# Py ATB
+# Attributes - name, id, phone no, gender, coor_eyes, city, location, address
+# BY which you are recognized
+# Pythin Java, C++
+
+# Behavior - walk, talk, sleep, cry, smile, sing, dance - some action
+# you can do something /methods 
+
+# We need a PL that is close to real world
+
